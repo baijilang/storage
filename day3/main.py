2d89fa8d46798d1c08acca6fa101dea30b01d5b2
baijilang <92006158+baijilang@users.noreply.github.com>
2021-10-11 09:57:20 +0800

    Add files via upload
    day3_1

--- day3/main.py
+++ day3/main.py
@@ -3,16 +3,36 @@
 随机生成选择一人
 随机生成处罚遍数
 
-优化代码：一个输入进行判断，可预先输入参与者，完成后可以从以上随机选人受罚
+优化代码：一个输入进行判断，可预先输入参与者，以及惩罚上限，输入数字可以从以上随机选人受罚，支持中途退出
 '''
 import random
+print("========惩罚游戏========\ntips1.输入任何纯数字开始选人，并随机生成处罚遍数\ntips2.输入q或回车退出游戏")
+names=[]  #定义一个数组，接收输入的参与人
+global name
+name=1
+TIME=input("请输入最大惩罚次数:")
 while True:
-    name=input("请输入代号：")
-    
-    if a == " q ":
+    if TIME.isdigit() and (name!="q" or name!=""):
+        while True:
+            name=input("请输入代号：")
+            if name == "q" or name=="" :
+                break
+            elif name.isdigit():
+                if len(names)!=0:
+                    ran_index=random.randint(0,len(names)-1)
+                    ran_times=random.randint(0,int(TIME))
+                    print("%s接受惩罚%d次"%(names[ran_index],ran_times))
+                else:
+                    print("没有参与者")
+            elif name=="list":
+                print(names)
+            else:
+                names.append(name)
+                continue
         break
-    elif a == "go":
-    else:
-        continue
+    elif (TIME=="q" or TIME=="") or (name=="q" or name==""):
+        break
+    else :
+        TIME=input("请重输惩罚次数：")
 
 
---
+++ day3/test.py
@@ -0,0 +1,31 @@
+
+# import random
+# list=["陆梓言","郭洪波","方则属","签","千纸鹤","EXO"]
+# print("============欢迎来到处罚系统===============")
+# while 1==1:
+#     inex=input("输入1开始选人")
+#     ran=random.randint(0,len(list)-1)#len==6  范围是0-6全部可以取到
+#     inex1=input("输入2开始生成处罚编数")
+#     num=random.randint(0,90)
+#     print(list[ran],"处罚了",num,"遍")
+# # a=[12,2]
+# names=a
+# print(names)
+
+'''
+添加 list.append  .extend  .insert(A,B)
+删除 list.remove() 删除list里面特定的元素
+    del list[n] 删除list的角标为n的元素
+    N=m.pop 将列表最后一个元素返回赋值给N,并删除列表中的该元素
+列表分片 N=list[A:B] N得到list中角标为A到B的元素
+
+'''
+c=['a','b']
+ch=3
+d=[1,2]
+e=[]
+for w  in c : print(c)
+for q  in d : print(d)
+print(e)
+e.append(d)
+print(e)
